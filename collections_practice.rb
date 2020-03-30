80ecd84bff3193e9b165e4700d397dabd0068f72
edmundo papi <edmundopierre@yahoo.com>
2020-03-30 19:26:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
 
+   return array.sort
+end
+
+def sort_array_desc(array)
+  return array.sort.reverse
+
+end
+
+def sort_array_char_count(array)
+
+    array.sort_by do |value|
+       [value.length,value]
+
+    end
+
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1]= array[2]
+  array[2]=temp
+  return array
+end
+
+def reverse_array(array)
+
+ return array.reverse
+
+end
+
+def  kesha_maker(array)
+
+   empty =[]
+   array.each do |value|
+      value[2] = "$"
+      empty << value
+   end
+      return empty
+end
